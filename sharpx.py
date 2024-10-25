a801248493971aebd407d1f07367276f5a3a99de
Sharpddos33 <jounhaja069@gmail.com>
2024-10-25 23:11:21 +0530

    Update sharpx.py

--- sharpx.py
+++ sharpx.py
@@ -2,13 +2,13 @@
 #SCRIPT BY SharpX72
   
 #Enter Your Bot Token here get it from @botfather
-BOT_TOKEN=("7937963960:AAFA0IzNwtvOpHo-F5eRu9G-oAeZjormTL0")
+BOT_TOKEN=("7530209253:AAHuqAzGInVIlgkiui8vTUIoFU6_apXcsRY")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("KHILADI_OP")
+OWNER_USERNAME=("KOH_AKASH")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("898594603")
+ADMIN_IDS=("1999262276")
 
 
 
