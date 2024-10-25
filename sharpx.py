3cc80da18ca18bf975c7f12863d445da85341473
Sharpddos33 <jounhaja069@gmail.com>
2024-10-25 23:08:12 +0530

    Add files via upload

---
+++ sharpx.py
@@ -0,0 +1,17 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY SharpX72
+  
+#Enter Your Bot Token here get it from @botfather
+BOT_TOKEN=("7937963960:AAFA0IzNwtvOpHo-F5eRu9G-oAeZjormTL0")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("KHILADI_OP")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("898594603")
+
+
+
+
+
+  
